864e3e7269ccc4d893f3f26218e06bb13c81e282
kmile <k.milev@yahoo.com>
2022-04-30 16:20:53 +0300

    SocketModule Activated

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { SocketModule } from './socket/socket.module';
 
 @Module({
-  imports: [],
+  imports: [SocketModule],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/socket/socket.module.ts
+++ src/socket/socket.module.ts
@@ -1,9 +1,9 @@
 import { Module } from '@nestjs/common';
 import { SocketGateway } from './socket.gateway';
-import { MetaTraderModule} from '../meta-trader/meta-trader.module';
+import { MetaTraderModule } from '../meta-trader/meta-trader.module';
 
 @Module({
-  imports:[MetaTraderModule],
-  providers: [SocketGateway]
+  imports: [MetaTraderModule],
+  providers: [SocketGateway],
 })
 export class SocketModule {}
