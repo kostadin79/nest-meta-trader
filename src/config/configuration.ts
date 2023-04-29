f20d2860fca8d87777025c7bbff2de5539e62518
kmile <k.milev@yahoo.com>
2023-04-29 12:42:06 +0300

    Configuration - Socket environment variable fix

--- src/config/configuration.ts
+++ src/config/configuration.ts
@@ -2,6 +2,6 @@ export default () => ({
   metaTraderSocket:{
     apiKey: process.env.API_KEY || 'CHANGEME',
     reqUrl: process.env.REQ_URL || 'tcp://127.0.0.1:5555',
-    pullUrl: process.env.DATABASE_PORT || 'tcp://127.0.0.1:5556',
+    pullUrl: process.env.PULL_URL || 'tcp://127.0.0.1:5556',
   }
 });
